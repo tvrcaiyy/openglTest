b8158d75c82530e1c2d963b775475beab35a50f3
tvrcaiyy <274510839@qq.com>
2019-08-15 21:28:30 +0800

    dof
    submit test

--- OpenglTest/Camera.h
+++ OpenglTest/Camera.h
@@ -7,7 +7,7 @@
 #include <GL/glew.h>
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
-
+//#include
 
 
 // Defines several possible options for camera movement. Used as abstraction to stay away from window-system specific input methods
