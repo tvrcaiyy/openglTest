6f0c69fdb7ad22f590853bde1b3b34742361525c
tvrc <274510839@qq.com>
2019-08-21 08:07:40 +0800

    hellotriangle

--- HelloTriangle/HelloTriangle.cpp
+++ HelloTriangle/HelloTriangle.cpp
@@ -1,26 +1,24 @@
-#include "glad/glad.h"
-#include "GLFW/glfw3.h"
+#include "glad\glad.h"
+#include "GLFW\glfw3.h"
 #include <iostream>
 
-void framebuffer_size_callback(GLFWwindow* pWindow, int width,int height);
-void processInput(GLFWwindow* pWindow);
-void display();
+const unsigned int SRC_WIDTH = 800;
+const unsigned int SRC_HEIGHT = 600;
 
-// settings
-const unsigned int SCR_WIDTH = 800;
-const unsigned int SCR_HEIGHT = 600;
+void framebuffer_size_callback(GLFWwindow* pWindow, int width,int height);
+
 
 int main()
 {
 	glfwInit();
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	GLFWwindow* pWindow = glfwCreateWindow(SCR_WIDTH,SCR_HEIGHT,"Hello Window",NULL,NULL);
+	GLFWwindow* pWindow = glfwCreateWindow(SRC_WIDTH,SRC_HEIGHT,"HelloTriangle",NULL,NULL);
 	if(pWindow == NULL)
 	{
-		std::cout << "create window failed !" << std::endl;
+		std::cout << "create window failed !!" << std::endl;
 		glfwTerminate();
 	}
 	glfwMakeContextCurrent(pWindow);
@@ -30,38 +28,15 @@ int main()
 		std::cout << "Failed to initilize GLAD !" << std::endl;
 		return -1;
 	}
-
-	glViewport(0,0,800,600);
+	
+	glViewport(0,0,SRC_WIDTH,SRC_HEIGHT);
 
 	glfwSetFramebufferSizeCallback(pWindow,framebuffer_size_callback);
 
-	while (!glfwWindowShouldClose(pWindow))
-	{
-		processInput(pWindow);
-
-		display();
-
-		glfwPollEvents();
-		glfwSwapBuffers(pWindow);
-	}
-
-	glfwTerminate();
 	return 0;
 }
 
 void framebuffer_size_callback(GLFWwindow* pWindow, int width,int height)
 {
 	glViewport(0,0,width,height);
-}
-
-void processInput(GLFWwindow* pWindow)
-{
-	if(glfwGetKey(pWindow,GLFW_KEY_ESCAPE) == GLFW_TRUE)
-		glfwSetWindowShouldClose(pWindow,true);
-}
-
-void display()
-{
-	glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-	glClear(GL_COLOR_BUFFER_BIT);
 }
